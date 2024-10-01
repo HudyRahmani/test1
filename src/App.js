2c3001158fcbbd19373b6ec8a2a8e37fb4b8d115
Hudy Rahmani <hudyrahmani@gmail.com>
2024-10-01 22:47:51 +0430

    add username and email address

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ 
+import './App.css'
+function App() {
+  
+  return (
+    <div className={`App`}>
+        <input type='text' />
+        <h2>Second change </h2>
+    </div>
+  );
+}
+
+export default App;
